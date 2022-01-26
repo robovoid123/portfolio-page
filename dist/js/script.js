0a554d0556f2fe1734735d1829f0abdb40cf2bc7
Abinash Shrestha <abinashvoidshrestha@gmail.com>
2022-01-26 19:16:02 +0545

    window class complete, back to css, window in dom changed to new version

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,7 +34,7 @@
 
 (function reduceDescriptionText() {
   const projectDesc = document.querySelectorAll(".project__desc p");
-  const MAX_TEXT = 60;
+  const MAX_TEXT = 90;
   projectDesc.forEach((desc) => {
     let text = desc.innerHTML;
     if (text.length > MAX_TEXT) {
@@ -49,130 +49,8 @@
   });
 })();
 
-(function windowActions() {
-  const windowCloseBtn = document.querySelectorAll(".close");
-  const windowMinBtn = document.querySelectorAll(".minimize");
-  const windowFullBtn = document.querySelectorAll(".fullscreen");
-  const windowHeader = document.querySelectorAll(".window-style");
-
-  // TODO: add functionality
-  windowFullBtn.forEach((btn) => {
-    btn.addEventListener("click", () => console.log("fullscreen"));
-  });
-
-  // TODO: add functionality
-  windowMinBtn.forEach((btn) => {
-    btn.addEventListener("click", () => console.log("minimize"));
-  });
-
-  windowCloseBtn.forEach((btn) => {
-    btn.addEventListener(
-      "click",
-      () =>
-        (btn.parentElement.parentElement.parentElement.style = "display: none")
-    );
-  });
-
-  windowHeader.forEach((header) => {
-    const elmnt = header.parentElement;
-
-    header.addEventListener("mousedown", (e) => {
-      elmnt.style.zIndex = 300;
-      // get the mouse cursor position at startup:
-      let prevX = e.clientX;
-      let prevY = e.clientY;
-
-      window.addEventListener("mouseup", closeDragElement);
-      window.addEventListener("mousemove", elementDrag);
-
-      function elementDrag(e) {
-        // calculate the new cursor position:
-        let deltaX = prevX - e.clientX;
-        let deltaY = prevY - e.clientY;
-        prevX = e.clientX;
-        prevY = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = elmnt.offsetTop - deltaY + "px";
-        elmnt.style.left = elmnt.offsetLeft - deltaX + "px";
-      }
-
-      function closeDragElement() {
-        // stop moving when mouse button is released:
-        window.removeEventListener("mouseup", closeDragElement);
-        window.removeEventListener("mousemove", elementDrag);
-        elmnt.style.zIndex = 299;
-      }
-    });
-  });
-})();
-
-// class Window {
-//   isResizing = false;
-//   isMinimized = false;
-//   isClosed = false;
-//   isFullScreen = false;
-
-//   constructor(domWindow) {
-//     this.domWindow = domWindow;
-//     this.domHeader = domWindow.children[0]; // first element always header
-//     this.domCloseBtn = this.domHeader.children[0];
-//     this.domMinimizeBtn = this.domHeader.children[1];
-//     this.domFullScreenBtn = this.domHeader.children[2];
-
-//     // adding eventListners
-//     this.domCloseBtn.addEventListener("click", this.close);
-//     this.domMinimizeBtn.addEventListener("click", this.minimize);
-//     this.domFullScreenBtn.addEventListener("click", this.makeFullScreen);
-
-//     this.domHeader.addEventListener("mousedown", this.startDrag);
-//   }
-
-//   startDrag(event) {
-//     this.domWindow.style.zIndex = 300;
-
-//     // get the mouse cursor position at startup:
-//     this.prevX = e.clientX;
-//     this.prevY = e.clientY;
-
-//     window.addEventListener("mouseup", this.finishDrag);
-//     window.addEventListener("mousemove", this.drag);
-//   }
-
-//   drag(event) {
-//     // calculate the new cursor position:
-//     let deltaX = this.prevX - event.clientX;
-//     let deltaY = this.prevY - event.clientY;
-//     this.prevX = e.clientX;
-//     this.prevY = e.clientY;
-
-//     // set the element's new position:
-//     this.domWindow.style.top = this.domWindow.offsetTop - deltaY + "px";
-//     this.domWindow.style.left = this.domWindow.offsetLeft - deltaX + "px";
-//   }
-
-//   finishDrag(event) {
-//     // stop moving when mouse button is released:
-//     window.removeEventListener("mouseup", this.finishDrag);
-//     window.removeEventListener("mousemove", this.drag);
-
-//     // send not moving window to back
-//     this.domWindow.style.zIndex = 299;
-//   }
-
-//   close(event) {
-//     this.domWindow.style = "display: none";
-//   }
-
-//   minimize(event) {}
-//   resize(event) {}
-//   makeFullScreen(event) {}
-// }
-
-// let windowObjList = [];
-// domWindows.forEach((window) => {
-//   windowObjList.append(new Window(window));
-// });
-
+// window in dom should be like this
+// container should have height and width specified
 // <div class="header-top-container">
 //   <div class="window">
 //     <div className="window-header">
@@ -185,3 +63,77 @@
 //     </div>
 //   </div>
 // </div>
+
+class Window {
+  isResizing = false;
+  isMinimized = false;
+  isClosed = false;
+  isFullScreen = false;
+  domWindow = null;
+
+  constructor(domWindow) {
+    this.domWindow = domWindow;
+    this.domHeader = domWindow.children[0]; // first element always header
+    this.domCloseBtn = this.domHeader.children[0].children[0];
+    this.domMinimizeBtn = this.domHeader.children[0].children[1];
+    this.domFullScreenBtn = this.domHeader.children[0].children[2];
+
+    // adding eventListners
+    this.domCloseBtn.addEventListener("click", this.close.bind(this));
+    this.domMinimizeBtn.addEventListener("click", this.minimize.bind(this));
+    this.domFullScreenBtn.addEventListener(
+      "click",
+      this.makeFullScreen.bind(this)
+    );
+
+    this.domHeader.addEventListener("mousedown", this.startDrag.bind(this));
+  }
+
+  startDrag(event) {
+    this.domWindow.style.zIndex = 300;
+
+    // get the mouse cursor position at startup:
+    this.prevX = event.clientX;
+    this.prevY = event.clientY;
+
+    // this.domWindow.addEventListener("mouseup", this.finishDrag.bind(this));
+    this.domWindow.onmouseup = this.finishDrag.bind(this);
+    this.domWindow.onmousemove = this.drag.bind(this);
+  }
+
+  drag(event) {
+    // calculate the new cursor position:
+    let deltaX = this.prevX - event.clientX;
+    let deltaY = this.prevY - event.clientY;
+    this.prevX = event.clientX;
+    this.prevY = event.clientY;
+
+    // set the element's new position:
+    this.domWindow.style.top = this.domWindow.offsetTop - deltaY + "px";
+    this.domWindow.style.left = this.domWindow.offsetLeft - deltaX + "px";
+  }
+
+  finishDrag(event) {
+    // stop moving when mouse button is released:
+    this.domWindow.onmouseup = null;
+    this.domWindow.onmousemove = null;
+
+    // send not moving window to back
+    this.domWindow.style.zIndex = 299;
+  }
+
+  close(event) {
+    this.domWindow.style = "display: none";
+  }
+
+  minimize(event) {}
+  resize(event) {}
+  makeFullScreen(event) {}
+}
+
+let windowObjList = [];
+const domWindows = document.querySelectorAll(".window");
+
+domWindows.forEach((window) => {
+  windowObjList.push(new Window(window));
+});
